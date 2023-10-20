46596d6d045d9454122c5a4e9dc022743af5bd1a
JosephTan13 <josephtan@lcad.edu>
2023-10-20 16:48:57 -0700

    fixed break problem

--- Assets/SupanthaPaul/2D Platformer Controller/Scripts/Camera/CameraFollow.cs
+++ Assets/SupanthaPaul/2D Platformer Controller/Scripts/Camera/CameraFollow.cs
@@ -15,7 +15,7 @@ namespace SupanthaPaul
 		public Vector3 minCamerabounds;
 		public Vector3 maxCamerabounds;
 
-		//public 
+		//public aiosnf
 		private bool isFollowing = true;
 
         private void FixedUpdate()
